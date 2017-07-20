2377ae5e6787d9b382845f0931739948c109c2c6
hurricanemad <blowings@126.com>
2017-07-20 21:18:36 +0800

    complete the alignment of person and background image

--- MergeImagegit/MergeImage/MergeImage/stdafx.h
+++ MergeImagegit/MergeImage/MergeImage/stdafx.h
@@ -14,14 +14,15 @@
 
 // TODO:  在此处引用程序需要的其他头文件
 #include<iostream>
-#include<opencv2/core.hpp>
-#include<opencv2/highgui.hpp>
-#include<opencv2/imgproc.hpp>
-#include<opencv2/imgcodecs.hpp>
-#include<opencv2/ximgproc.hpp>
+//#include<opencv2/core.hpp>
+//#include<opencv2/highgui.hpp>
+//#include<opencv2/imgproc.hpp>
+//#include<opencv2/imgcodecs.hpp>
+//#include<opencv2/ximgproc.hpp>
 #include <opencv2/xfeatures2d.hpp>
-#include <opencv2/features2d.hpp>
-
+//#include <opencv2/features2d.hpp>
+//#include <opencv2/flann.hpp>
+#include <opencv2/opencv.hpp>
 
 
 #define VERSION_ID CVAUX_STR(CV_VERSION_MAJOR) CVAUX_STR(CV_VERSION_MINOR) CVAUX_STR(CV_VERSION_REVISION)
@@ -39,7 +40,12 @@
 #pragma comment(lib, cvLib("ximgproc"))
 #pragma comment(lib, cvLib("xfeatures2d"))
 #pragma comment(lib, cvLib("features2d"))
+#pragma comment(lib, cvLib("flann"))
+#pragma comment(lib, cvLib("objdetect"))
+#pragma comment(lib, cvLib("video"))
 
 using namespace cv;
 using namespace cv::xfeatures2d;
 using namespace std;
+
+const double dDistRatio = 1.0 / 1.5;
---
+++ MergeImagegit/readme.txt
@@ -0,0 +1,5 @@
+Project: MergeImage
+Author: Hui Shen
+Additional Dependencies:OpenCV 3.1, Visual studio 2015
+Abstract: 
+The project aligns the person and background images and merge the region containing the person with background image.
